7b5a8f242069fb01657e2c31820a693230a0803b
Artur Romaniuk <01159409@pw.edu.pl>
2020-12-04 16:55:46 +0100

    Added resource video, starting to work on tracking

--- NumberPlatesReco/main.cpp
+++ NumberPlatesReco/main.cpp
@@ -18,8 +18,10 @@ int main()
 		cout << "Fatal ERROR! Could not open video file"<<source<<endl;
 		return 1;
 	}
-		
-
+	
+	Rect trac_frame(300, 200, 100, 50);
+	//MAT roi = frame(trac)
+	
 	while (true)
 	{
 		Mat frame;
