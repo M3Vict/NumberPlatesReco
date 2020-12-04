6bc514db3742f8b3eb736094597f73500fd5ea23
Artur Romaniuk <01159409@pw.edu.pl>
2020-12-04 16:50:43 +0100

    Video input and display

--- NumberPlatesReco/main.cpp
+++ NumberPlatesReco/main.cpp
@@ -1,21 +1,36 @@
-﻿#include <opencv2/opencv.hpp>
+﻿#include <iostream>
+#include <string>
+
+
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
+	string source= "C:\\Users\\Admin\\Desktop\\Garmin Dash Cam 55 Plus Sample Footage.mp4";
+	//cin >> source;
 
-	VideoCapture cap(0);
-	if (!cap.isOpened())
+	VideoCapture video(source);
+	if (!video.isOpened())
+	{
+		cout << "Fatal ERROR! Could not open video file"<<source<<endl;
 		return 1;
-	for (;;)
+	}
+		
+
+	while (true)
 	{
 		Mat frame;
-		cap >> frame;
-		imshow("Webcam", frame);
-
-		if (waitKey(30) >= 0)
+		video >> frame;
+		if (frame.empty())
 			break;
+		imshow("Video", frame);
+		int keyboard = waitKey(30);
+		if (keyboard == 'q' || keyboard == 27)
+			break;
+		
 	}
 
 	return 0;
